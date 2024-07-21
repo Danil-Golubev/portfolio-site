5c2ed542990ab5bffc4665a6eb3406a476a58620
Danil-Golubev <dokers190rus@mail.ru>
2024-07-21 19:44:57 +0300

    createdproject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
+import {AnimatedModel} from './Animatedmodel';
 
-function App() {
+const Model = ({ url }) => {
+  const group = useRef();
+  const { scene, animations } = useGLTF(url);
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    if (actions && Object.keys(actions).length > 0) {
+      actions[Object.keys(actions)[0]].play();
+    }
+  }, [actions]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <group ref={group} dispose={null}>
+      <primitive object={scene} />
+    </group>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Canvas
+       camera={{ position: [2, 0, 12.25], fov: 15 }}
+       style={{
+        backgroundColor: '#111a21',
+        width: '1000px',
+        height: '1000px',
+     }}
+       >
+
+         <ambientLight intensity={0.1} />
+         <directionalLight intensity={0.4} />
+      <pointLight position={[10, 10, 10]} />
+ <Model url ='/models/animatedmodel.glb'/>
+     { /*<AnimatedModel />*/}
+      <OrbitControls />
+    </Canvas>
+
+
+  );
+};
 
 export default App;
