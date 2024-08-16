b634ab9af4d7d03216a91bb440c3748b91b43eb4
Danil-Golubev <dokers190rus@mail.ru>
2024-08-16 23:51:39 +0300

    123

--- src/components/WidgetBar/WidgetBar.jsx
+++ src/components/WidgetBar/WidgetBar.jsx
@@ -6,6 +6,7 @@ export const WidgetBar = () => {
 			<div className={styles.widgetBar}>
 				<div className={styles.widgetContent}>
 					<div className={styles.titlesBlock}>
+						<img className={styles.svg} alt='' src='/public/logo192.png' />
 						<h2 className={styles.fontWidget}>Hi, my name is Danil</h2>
 						<p className={styles.fontWidget}> and that tab is your guider, just keep it scrolling</p>
 					</div>
