08679e49b8acdd3f3f9e27c7a86a6412ae827472
Danil-Golubev <dokers190rus@mail.ru>
2024-08-27 23:41:53 +0300

    daily fix

--- src/components/WidgetBar/WidgetBar.jsx
+++ src/components/WidgetBar/WidgetBar.jsx
@@ -5,7 +5,9 @@ export const WidgetBar = () => {
 		<>
 			<div className={styles.mainBlock}>
 				<div className={styles.contentBlock}>
-					<div className={styles.title}>Hello</div>
+					<div className={styles.title}>Danil Golubev</div>
+					<div className={styles.title}>Frontend software developer</div>
+					<div className={styles.title}>@ownantwerp</div>
 				</div>
 			</div>
 		</>
